852068661e818e792971855f5abf53c18e6e29e9
Naoki Takahashi <naoki.tkb@gmail.com>
2022-11-11 20:31:01 +0900

    update: prettier option `singleAttributePerLine` to `true`

--- .prettierrc.js
+++ .prettierrc.js
@@ -4,6 +4,7 @@ const config = {
     singleQuote: true,
     tabWidth: 4,
     useTabs: false,
+    singleAttributePerLine: true,
     overrides: [
         {
             files: ['*.{json,yml,yaml}'],
